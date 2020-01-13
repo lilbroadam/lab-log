00511f4e9f2741a54db80c2df26726b273fccd95
lilbroadam <lilbroadam@gmail.com>
2020-01-12 19:32:39 -0600

    Removed default username functionality
    - program no longer maintains a default user name
      - info.lablog no longer stores a default username and instead stores a
    comment line to indicate username recording, then on the next line the number of usernames, then each
    username on their own line
    - changed the open_info_file to return a pointer to a string array of
    usernames
    - added a login function to handle the login process but it hasn't been
    implemented yet

--- lablog.c
+++ lablog.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+void login(char *, char *[], int);
+
 FILE * infoFile = NULL;
 FILE * tempInfoFile = NULL;
 FILE * logFile = NULL;
@@ -17,15 +19,16 @@ int main(int argc, char* argv[]){
 		print_help_menu();
 
 
-	char username[USERNAMEBUFFERSIZE];
-	open_info_file(tempInfoFile, infoFile, username);
-	// write_default_username(tempInfoFile, username);
-
+	int numUsernames;
+	char ** usernames = open_info_file(tempInfoFile, infoFile, &numUsernames);
 	logFile = fopen(LOGFILE, "r");
 	tempLogFile = fopen(TEMPLOGFILE, "r");
 
 	if(strcmp("login", argv[1]) == 0){
+		if(argv[2] == NULL)
+			print_help_menu();
 
+		login(argv[2], usernames, numUsernames);
 	}else if(strcmp("logout", argv[1]) == 0){
 
 	}else if(strcmp("drive", argv[1]) == 0){
@@ -45,54 +48,89 @@ int main(int argc, char* argv[]){
 	cleanup_files(infoFile, tempInfoFile, logFile, tempLogFile);
 }
 
-// open the info file and the temp info file and return the default username read from the file
-void open_info_file(FILE * tempInfoFile, FILE * infoFile, char usernameDestination[]){
+void login(char * username, char * usernames[], int numUsernames){
+
+
+
+}
+
+
+
+// open the info file and temp info file, return the number of usernames, and pass an array of usernames from the info file
+// TODO the signature of this function has a bad design; make it better
+char ** open_info_file(FILE * tempInfoFile, FILE * infoFile, int * numUsernamesDest){
 	// check if temporary info file already exists
 	if(access(TEMPINFOFILE, F_OK) == false){
 		// TODO
 		fprintf(stderr, "temporary file was found at start of program, implement this recovery\n");
 	}
 
+	char ** usernamesDestination;
 	infoFile = fopen(INFOFILE, "r");
-	char defaultUsername[USERNAMEBUFFERSIZE] = "";
-	if(infoFile == NULL){ // if the info file doesn't exi
+	if(infoFile == NULL){ // if the info file doesn't exist
 		fprintf(stderr, "File \"%s\" not found, creating file...\n", INFOFILE);
 
 		infoFile = fopen(INFOFILE, "w"); // create the info file
 		if(infoFile == NULL){
-			fprintf(stderr, "Couldn't open %s\n", INFOFILE);
+			fprintf(stderr, "Couldn't create %s\n", INFOFILE);
 			exit(1);
 		}
 
-		printf("Please provide a default username: ");
-		fgets(usernameDestination, USERNAMEBUFFERSIZE, stdin);
+		printf("Please provide a username: ");
+		char * providedUsername = malloc(USERNAMEBUFFERSIZE);
+		fgets(providedUsername, USERNAMEBUFFERSIZE, stdin);
+
+		fputs("# usernames\n", infoFile);
+		fputs("1\n", infoFile);
+		fputs(providedUsername, infoFile);
+		fputs("\n", infoFile);
+		fclose(infoFile);
+
+		printf("%s created. Please try your previous command again.\n", INFOFILE);
+		exit(0);
 	} else {
-		// read the first comment line in the info file
-		char * fileLine = fgets(defaultUsername, USERNAMEBUFFERSIZE, infoFile);
+		// read the usernames comment line in the info file
+		char buffer[USERNAMEBUFFERSIZE + 1] = "\0";
+		char * fileLine = fgets(buffer, USERNAMEBUFFERSIZE, infoFile);
 		if(fileLine == NULL)
 			file_read_error(INFOFILE);
 
-		// get the default user name from the info file and output it
-		fileLine = fgets(defaultUsername, USERNAMEBUFFERSIZE, infoFile);
+		// get the number of usernames from the info file
+		fileLine = fgets(buffer, USERNAMEBUFFERSIZE, infoFile);
 		if(fileLine == NULL)
 			file_read_error(INFOFILE);
-		strcpy(usernameDestination, fileLine);
+		int numUsernames = atoi(buffer);
+		*numUsernamesDest = numUsernames;
+
+		// get the usernames from the info file and place them into usernamesDestination[]
+		usernamesDestination = malloc((numUsernames + 1) * sizeof(char *)); // + 1 to alloc space if a new username is registered
+		for(int i = 0; i < numUsernames; i++){
+			if(fgets(buffer, USERNAMEBUFFERSIZE, infoFile) == NULL)
+				file_read_error(INFOFILE);
+
+			char * username = malloc(USERNAMEBUFFERSIZE * sizeof(char));
+
+			strcpy(username, buffer);
+			usernamesDestination[i] = username;
+
+			// remove the '\n' from the username string
+			int j = 0;
+			while(username[j] != '\n')
+				j++;
+			username[j] = '\0';
+		}
 
 		// TODO read past next line to set up for the next read
 	}
 
 	// copy info file into temp infofile
 	rewind(infoFile);
-	char buffer[100] = "";
+	char buffer[100] = ""; // TODO change this magic number to a constant
 	tempInfoFile = fopen(TEMPINFOFILE, "w");
-	while(fgets(buffer, 100, infoFile) != NULL){
+	while(fgets(buffer, 100, infoFile) != NULL)
 		fputs(buffer, tempInfoFile);
-	}
-}
 
-void write_default_username(FILE * outputFile, char username[]){
-	fprintf(outputFile, "# default username\n");
-	fprintf(outputFile, "%s", username);
+	return usernamesDestination;
 }
 
 // close file pointers, overwrite old .lablog files, delete temp files
--- lablog.h
+++ lablog.h
@@ -7,7 +7,7 @@
 #define TEMPLOGFILE "log.lablog.tmp"
 #define USERNAMEBUFFERSIZE 30
 
-void open_info_file(FILE *, FILE *, char[]);
+char ** open_info_file(FILE *, FILE *, int *);
 void write_default_username(FILE *, char[]);
 void cleanup_files(FILE *, FILE *, FILE *, FILE *);
 
