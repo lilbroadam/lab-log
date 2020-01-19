0d421837a44994633bcc03e66fcdb70e41e8a44b
lilbroadam <lilbroadam@gmail.com>
2020-01-19 14:31:06 -0600

    Added commandhandler.c to handle commands like login, logout, etc

---
+++ commandhandler.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void login(char * username, char * usernames[], int numUsernames){
+	printf("login\n");
+}
+void logout(){
+
+}
+
+void drive(){
+
+}
+
+void toggleBreak(){
+
+}
+
+void print(){
+
+}
+
+void report(){
+
+}
+
+void config(){
+
+}
+
+
+// void login(char * username, char * usernames[], int numUsernames){}
---
+++ commandhandler.h
@@ -0,0 +1,7 @@
+void login(char *, char *, int)();
+void logout();
+void drive();
+void toggleBreak();
+void print();
+void report();
+void config();
